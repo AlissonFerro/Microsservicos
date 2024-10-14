6a0e622b935f9738646763a541830e26c5370da6
Ferro Alisson (CtP/ETS) <fel9ct@bosch.com>
2024-10-14 09:30:15 -0300

    add new MS

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,18 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/api/users', createProxyMiddleware({
+app
+  .use('/api/users', createProxyMiddleware({
     target: 'http://localhost:4001/api/users',
     changeOrigin: true,
-  }));
+  }))
+  .use('/api/store', createProxyMiddleware({
+    target: 'http://localhost:4002/api/store',
+    changeOrigin: true,
+  }))
 
-app.use(express.json());
-
-app.use(handleError as any);
+  .use(express.json())
+  .use(handleError as any)
 
 
 app.listen(port, () => console.log(`Server running in ${port}`));
