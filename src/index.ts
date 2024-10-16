25241f8e4ac48793ede62a609b22fceb3574c2ef
Ferro Alisson (CtP/ETS) <fel9ct@bosch.com>
2024-10-16 13:50:35 -0300

    change variable res to _

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
+app.use((_, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
